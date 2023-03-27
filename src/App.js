0f4d336cd0cf6f3a85be8f848fbe30b79b619e74
Connor Kormos <connorkormos97@gmail.com>
2023-03-27 13:37:57 -0700

    original fetch request functional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
+
+const CLIENT_ID = "1ff422b13da04c47b1d3639000b11abb";
+const CLIENT_SECRET = "403561469b9c409faa37c5f49d39c46e";
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+
+// Fetch Access Token and set to state
+  useEffect(() => {
+      let authParameters = {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: "grant_type=client_credentials&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET
+      }
+      fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(resp => resp.json())
+      .then(data => setAccessToken(data.access_token))
+    }, [])
+
+    // Search
+    async function search() {
+      console.log("Search for " + searchInput);
+
+      // Get request using search to get the Artist ID
+      let searchParameters = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + accessToken
+        }
+      }
+      let artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
+        .then(response => response.json())
+        .then(data => { return data.artists.items[0].id })
+  
+        console.log("Artist ID is " + artistID)
+        
+      // Get request with Artist ID to grab all the albums from that artist
+      let returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setAlbums(data.items)
+        })
+      // Display those albums to the user
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Search for Artist"
+            type="input"
+            onKeyDown={event => {
+              if (event.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={event => setSearchInput(event.target.value)}
+          />
+          <Button onClick={search}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          {albums.map( (album, i) => {
+            return (
+              <Card>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            )
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
